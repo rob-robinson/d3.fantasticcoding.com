e4e11574d088e56cde60982869835f11f5758ca5
Rob Robinson <robrobinsoncsuf@gmail.com>
2014-09-10 21:10:03 -0700

    initial commit...

---
+++ www/js/app.js
@@ -0,0 +1,22 @@
+var w = 300;
+var h = 100;
+var padding = 2;
+var dataset = [4,6,8,9,10];
+var svg = d3.select("body").append("svg")
+    .attr("width",w)
+    .attr("height",h);
+
+svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("x",function(d,i){
+        return i * (w / dataset.length);
+    })
+    .attr("y",function(d){
+        return h - (d);
+    })
+    .attr("width",w/dataset.length - padding)
+    .attr("height",function(d){
+        return d;
+    })
